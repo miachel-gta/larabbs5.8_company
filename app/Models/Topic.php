ef293f516ac64aee1951f5e64eb9acbd82819f49
vagrant <vagrant@homestead>
2020-04-14 09:01:54 +0000

    翻译 URL 标示

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -6,15 +6,18 @@ class Topic extends Model
 {
     protected $fillable = ['title', 'body', 'category_id', 'excerpt', 'slug'];
 
+
     public function category()
     {
         return $this->belongsTo(Category::class);
     }
 
+
     public function user()
     {
         return $this->belongsTo(User::class);
     }
+
 
     public function scopeWithOrder($query, $order)
     {
@@ -31,6 +34,7 @@ class Topic extends Model
         return $query->with('user', 'category');
     }
 
+
     public function scopeRecentReplied($query)
     {
         // 当话题有新回复时，我们将编写逻辑来更新话题模型的 reply_count 属性，
@@ -39,9 +43,16 @@ class Topic extends Model
         return $query->orderBy('updated_at', 'desc');
     }
 
+
     public function scopeRecent($query)
     {
         // 按照创建时间排序
         return $query->orderBy('created_at', 'desc');
+    }
+
+
+    public function link($params = [])
+    {
+        return route('topics.show', array_merge([$this->id, $this->slug], $params));
     }
 }
